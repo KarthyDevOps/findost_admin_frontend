60ffc2535d61e83ffc042c00f3790eb44f21efb3
Raja Kumaran <rajakumaran.doodleblue@gmail.com>
2023-06-07 19:02:25 +0530

    update

--- src/component/Dashboard/StaffManagement/index.js
+++ src/component/Dashboard/StaffManagement/index.js
@@ -1,24 +1,28 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect, Fragment, useCallback } from "react";
 import TableComp from "../../common/TableComp/TableComp";
 import InputBox from "component/common/InputBox/InputBox";
 import { useForm } from "react-hook-form";
 import NormalButton from "component/common/NormalButton/NormalButton";
 import "./style.scss";
-import { history } from "helpers";
+import { history, debounceFunction } from "helpers";
 import { BsSearch } from "react-icons/bs";
 import DropDown from "component/common/DropDown/DropDown";
 import { getStaffList, deleteStaff } from "service/Auth";
 import DeleteModal from "component/common/DeleteModal/DeleteModal";
 import { Toast } from "service/toast";
-import { useLocation } from "react-router-dom";
+import Loader from "component/common/Loader";
+import CustomController from "component/common/Controller";
+import NormalMultiSelect from "component/common/NormalMultiSelect";
 
 const StaffManagementComp = ({ create, view, edit, remove }) => {
-  const { register, handleSubmit, errors, reset, setError } = useForm({
+  const { register, handleSubmit, errors, reset, control } = useForm({
     mode: "onChange",
   });
 
   const [data, setData] = useState([]);
   const [searchStaff, setSearchStaff] = useState("");
+  const [role, setRole] = useState("");
+  const [status, setStatus] = useState("");
   const [pageCount, setPageCount] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
   const [active, setIsactive] = useState("");
@@ -26,7 +30,37 @@ const StaffManagementComp = ({ create, view, edit, remove }) => {
     id: null,
     show: false,
   });
-  const location = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const statusOptions = [
+    {
+      label: "ACTIVE",
+      value: "active",
+    },
+    {
+      label: "InACTIVE",
+      value: "inActive",
+    },
+  ];
+
+  const roleOptions = [
+    {
+      label: "SUPER_ADMIN",
+      value: "SUPER ADMIN",
+    },
+    {
+      label: "ADMIN",
+      value: "ADMIN",
+    },
+    {
+      label: "STAFF",
+      value: "STAFF",
+    },
+    {
+      label: "SUB_Admin",
+      value: "SUB ADMIN",
+    },
+  ];
 
   const includedKeys = [
     {
@@ -52,25 +86,41 @@ const StaffManagementComp = ({ create, view, edit, remove }) => {
   ];
 
   const getStaffListApi = async (page) => {
-    let params = {
-      page: page,
-      limit: 10,
-      search: "",
-    };
-    let response = await getStaffList(params);
-    if (response.status === 200) {
-      setIsactive(response?.data?.data?.list[0].isactive);
-      setData(response?.data?.data?.list);
-      setPageCount(response?.data?.data?.pageMeta?.pageCount);
-      setCurrentPage(response?.data?.data?.pageMeta?.currentPage);
+    try {
+      setIsLoading(true);
+      let params = {
+        page: page,
+        limit: 10,
+        search: searchStaff,
+        role: role,
+      };
+      if (status) {
+        status === "active"
+          ? (params.isActive = true)
+          : (params.isActive = false);
+      }
+      let response = await getStaffList(params);
+      if (response.status === 200 && response?.data?.data?.list) {
+        setIsactive(response?.data?.data?.list[0].isactive);
+        setData(response?.data?.data?.list);
+        setPageCount(response?.data?.data?.pageMeta?.pageCount);
+        setCurrentPage(response?.data?.data?.pageMeta?.currentPage);
+      } else {
+        setData([]);
+      }
+    } catch (e) {
+      console.log("e :>> ", e);
+    } finally {
+      setIsLoading(false);
     }
   };
+
   useEffect(() => {
     getStaffListApi(currentPage);
-  }, []);
+  }, [searchStaff, role, status]);
 
   const handlePageChange = (page) => {
-    setCurrentPage(page);
+    setCurrentPage(page.selected);
     getStaffListApi(page);
   };
 
@@ -95,6 +145,13 @@ const StaffManagementComp = ({ create, view, edit, remove }) => {
     setModalVisible({ show: false, id: null });
   };
 
+  const handleSearchChange = useCallback(
+    debounceFunction((value) => {
+      setSearchStaff(value);
+    }, 500),
+    []
+  );
+
   return (
     <Fragment>
       <div className="staff_table px-5 pt-2">
@@ -111,60 +168,100 @@ const StaffManagementComp = ({ create, view, edit, remove }) => {
                   name="search"
                   Iconic
                   value={searchStaff}
-                // onChange={(e) => {
-                //   setsearch(e.target.value);
-                //   setactivePage(1);
-                // }}
+                  onChange={(e) => handleSearchChange(e.target.value)}
                 />
                 <i className="search_iconic">
                   <BsSearch size={18} style={{ color: "#7E7E7E" }} />
                 </i>
               </div>
               <div className="col-md-3">
-                <DropDown
-                  // value={value}
-                  placeholder="Filter by Role"
-                // onChange={(e) => {}}
-                // options={options}
+                <CustomController
+                  name={"role"}
+                  control={control}
+                  error={errors?.role}
+                  defaultValue={role}
+                  rules={{ required: false }}
+                  render={({ onChange, ...fields }) => {
+                    return (
+                      <NormalMultiSelect
+                        {...fields}
+                        placeholder={"Select Role"}
+                        options={roleOptions}
+                        name="role"
+                        handleChange={(e, { value } = {}) => {
+                          onChange(value);
+                          setRole(value);
+                        }}
+                      />
+                    );
+                  }}
                 />
               </div>
-
               <div className="col-md-3">
-                <DropDown
-                  // value={value}
-                  placeholder="Filter by Status"
-                // onChange={(e) => {}}
-                // options={options}
+                <CustomController
+                  name={"status"}
+                  control={control}
+                  error={errors?.status}
+                  defaultValue={status}
+                  rules={{ required: false }}
+                  render={({ onChange, ...fields }) => {
+                    return (
+                      <NormalMultiSelect
+                        {...fields}
+                        placeholder={"Select Status"}
+                        options={statusOptions}
+                        name="status"
+                        handleChange={(e, { value } = {}) => {
+                          onChange(value);
+                          setStatus(value);
+                        }}
+                      />
+                    );
+                  }}
                 />
               </div>
             </div>
           </div>
-          {create && <div className="col-md-2 col-12 p-0 m-0">
-            <NormalButton
-              className="loginButton"
-              label={"Add Staff"}
-              onClick={() => {
-                localStorage.removeItem("editId");
-                history.push("/admin/staff-management/add-staff");
-              }}
-            />
-          </div>}
+          {create && (
+            <div className="col-md-2 col-12 p-0 m-0">
+              <NormalButton
+                className="loginButton"
+                label={"Add Staff"}
+                onClick={() => {
+                  localStorage.removeItem("editId");
+                  history.push("/admin/staff-management/add-staff");
+                }}
+              />
+            </div>
+          )}
         </div>
-        <div className="">
-          <TableComp
-            data={data}
-            isCheck={true}
-            EditAction={edit}
-            DeleteAction={remove}
-            includedKeys={includedKeys}
-            pageCount={pageCount}
-            onPageChange={handlePageChange}
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-            editRouteName={"/admin/staff-management/add-staff"}
-            handleOpenModal={handleOpenModal}
+        {console.log("data :>> ", data)}
+        {isLoading ? (
+          <Loader
+            loading={isLoading}
+            className="d-flex align-items-center justify-content-center"
           />
-        </div>
+        ) : data.length > 0 ? (
+          <div className="">
+            <TableComp
+              data={data}
+              isCheck={true}
+              EditAction={edit}
+              DeleteAction={remove}
+              includedKeys={includedKeys}
+              pageCount={pageCount}
+              onPageChange={handlePageChange}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+              editRouteName={"/admin/staff-management/add-staff"}
+              handleOpenModal={handleOpenModal}
+            />
+          </div>
+        ) : (
+          <div className="d-flex align-items-center justify-content-center mt-5 pt-5">
+            No Data Available
+          </div>
+        )}
         <div>
           {" "}
           <DeleteModal
