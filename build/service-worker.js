439335aa78655bfd47a53a53f8709ae06d0b8384
mohamed asik <mohamed.asik@doodle-blue.com>
2023-06-12 11:37:32 +0530

    update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b277a03eb10cdb5614b6ea28d234cc70.js"
+  "/precache-manifest.057bf2d02ebdf53beb63b07fa9067714.js"
 );
 
 self.addEventListener('message', (event) => {
